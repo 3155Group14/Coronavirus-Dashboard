6b082e9310f4e4469afa1dbcd986f9d740eab50c
lgilbe <lgilbe11@uncc.edu>
2020-11-24 21:37:37 -0500

    Removed unnecessary imports and cleaned up code.
    Removed unnecessary imports and cleaned up code.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import L from 'leaflet';
-
 import Layout from 'components/Layout';
 import Container from 'components/Container';
 import Map from 'components/Map';
-import Snippet from 'components/Snippet';
 import axios from 'axios';
-
 
 const LOCATION = {
   lat: 38.9072,
@@ -15,26 +11,18 @@ const LOCATION = {
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
-
 const IndexPage = () => {
-
-  /**
-   * mapEffect
-   * @description Fires a callback once the page renders
-   */
 
   async function mapEffect({ leafletElement: map } = {}) {
    let response;
-
       try {
         response = await axios.get('https://disease.sh/v3/covid-19/states');
       } catch(e) {
         console.log(`Failed to fetch states: ${e.message}`, e);
         return;
       }
-
       const { data = [] } = response;
-      console.log("Printing requested state data!");
+      console.log("Printing requested state data to console!");
       console.log(data);
   }
 
@@ -50,10 +38,8 @@ const IndexPage = () => {
       <Helmet>
         <title>Home Page</title>
       </Helmet>
-
       <Map  {...mapSettings}>
       </Map>
-
       <Container type="content" className="text-center home-start">
         <h2>Coronavirus-Dashboard</h2>
         <p>ITSC-3155 Group 14</p>
